437bf88f06cf4d1a890c4d3a8821db23926bb182
Marco Mendão <mac.mendao@gmail.com>
2021-10-25 13:04:08 +0100

    Add session 1

---
+++ 2_variables.py
@@ -0,0 +1,27 @@
+# Variable of type string
+# Strings store text
+var1 = "hello world"
+
+print("var1 =",var1)
+print("type(var1) =", type(var1))
+
+var2 = 2
+
+print("var2 =",var2)
+print("type(var2) =", type(var2))
+
+var3 = 4.0
+
+print("var3 =", var3)
+print("type(var3) =", type(var3))
+
+var4 = False
+var5 = True
+
+print("var4 =", var4)
+print("type(var5) =", type(var5))
+print("type(var4) =", type(var4))
+
+var6 = None
+print("var6 =", var6)
+print("type(var6) =", type(var6))
---
+++ 3_exercises1.py
@@ -0,0 +1,3 @@
+# 1. Create a variable that stores a string "hello world <your name>"
+# 2. Create a variable where you can store your age
+# 3. Create a variable where you can store your height
