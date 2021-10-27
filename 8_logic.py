7eef19ff322727613dac9e231af2d5dca47e0678
Marco Mendão <mac.mendao@gmail.com>
2021-10-27 13:01:30 +0100

    Session 8

---
+++ 8_logic.py
@@ -0,0 +1,53 @@
+# IF Statement
+
+# if condition
+# then logic
+# else logic
+
+marco = {
+    "name": "Marco",
+    "age": 31 ,
+    "size": 1.75
+}
+
+rui = {
+    "name": "Rui",
+    "age": 15,
+    "size": 1.20
+}
+
+person = rui
+
+if person["age"] >= 18:
+    print(f"{person['name']} is an Adult")
+    # print(marco["name"], " is an Adult")
+else:
+    print(f"{person['name']} is a child")
+
+# se person >= 2 alta
+# se person >= 1.6 e person < 2 normal
+# se person < 1.6 baixa
+
+if person["size"] >= 2.0:
+    print(f"{person['name']} is a tall person")
+elif person["size"] >= 1.6 and person["size"] <2.0:
+    print(f"{person['name']} is average")
+else:
+    print(f"{person['name']} is short")
+
+
+# Make a program that counts to 1-
+print("1")
+print("2")
+print("3")
+print("4")
+# ...
+
+# do then
+# while condition
+
+count = 1
+
+while count <= 10:
+    print(count)
+    count = count + 1
