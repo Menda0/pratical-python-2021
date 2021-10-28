5fb4e6d961cbe5e20f152b3e0468e989ec615441
Marco Mendão <mac.mendao@gmail.com>
2021-10-28 12:29:26 +0100

    Add exercise

---
+++ 11_exercices4.py
@@ -0,0 +1,9 @@
+'''
+    1. Create a function to calculate the area and perimeter of a square
+    1.1 P = 4 * L | A = L * L
+    2. Create a function to calculate the area of a triangle
+    2.1 A = B * H / 2
+'''
+
+def perimeterOfSquare(l):
+    return 4 * l
