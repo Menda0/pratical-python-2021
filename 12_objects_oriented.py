cef05aacdd5c02a0b8bd1d6560244368f836f591
Marco Mendão <mac.mendao@gmail.com>
2021-10-29 12:30:45 +0100

    Add franscisco example

--- 12_objects_oriented.py
+++ 12_objects_oriented.py
@@ -1,4 +1,5 @@
 
+# Francisco Example
 class Car:
 
     # attributes
--- 13_exercises5.py
+++ 13_exercises5.py
@@ -7,16 +7,56 @@
 ## 3.2 Eat() -> when athelete eats there must be an increse of one point of energy
 
 class Athlete:
-    pass
-    # energy
-    # name
 
-    # run()
-    # eat()
+    # attributes
+    # variables inside a class are called attributes
+    energy = 0
+    name = None
 
-manuel = Athlete()
-sandra = Athlete()
+    # constructor method
+    def __init__(self, energy, name):
+        self.energy = energy
+        self.name = name
 
+    # Methods
+    # Function inside a class are called methods
+    def run(self):
+        if self.energy >1:
+            self.energy = self.energy - 1
+            print(f"Person {self.name} run and energy:{self.energy}") 
+        else:
+            print(f"Person {self.name} not energy:{self.energy}")
+        
+        return self.energy
+
+    def eat(self):
+        self.energy = self.energy + 3
+        print(f"Person {self.name} eat and energy:{self.energy}") 
+
+    def runUntilFail(self):
+        while self.energy > 1:
+            self.run()
+
+
+    def __repr__(self):
+        return f"Text"
+
+sandra = Athlete(20, "Sandra")
+sandra.runUntilFail()
+
+manuel = Athlete(15, "Manuel")
+manuel.runUntilFail()
+
+'''
+count = 1
+while sandra.run() > 1:
+#while sandra.energy > 1:
+    #sandra.run()
+    count = count + 1
+    # if sandra.energy==0:
+    #   break
+
+sandra = Athlete(20, "Sandra")
 sandra.run()
-manuel.run()
-manuel.eat()
+sandra.eat()
+'''
