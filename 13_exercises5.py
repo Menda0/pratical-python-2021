3e208f91f11463dfc9a2d62239df1cfdb71e3c3c
Marco Mendão <mac.mendao@gmail.com>
2021-10-29 11:35:00 +0100

    Add exercise

---
+++ 13_exercises5.py
@@ -0,0 +1,22 @@
+## 1. Create a class called athlete
+## 2. The class athlete must have the following attributues:
+## 2.1 energy -> Int value that contains the athlete energy
+## 2.2 name -> Name of the athlete
+## 3. Athlete must have the following methods:
+## 3.1 Run() -> When athelete run there must be a decrease of one point of energy
+## 3.2 Eat() -> when athelete eats there must be an increse of one point of energy
+
+class Athlete:
+    pass
+    # energy
+    # name
+
+    # run()
+    # eat()
+
+manuel = Athlete()
+sandra = Athlete()
+
+sandra.run()
+manuel.run()
+manuel.eat()
