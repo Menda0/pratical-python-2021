d5aab5791b2f19df2ed66b8eacf9d5b90c9c352a
Marco Mendão <mac.mendao@gmail.com>
2021-11-03 10:47:34 +0000

    another commit

--- 16_execises6.py
+++ 16_execises6.py
@@ -1,13 +1,46 @@
 # https://api.spaceflightnewsapi.net/v3/articles
+# public apis https://github.com/public-apis/public-apis
 
 # 1. Use reuqest to get space news
 # 2. Write all the news inside a csv file
 # 3. id, title, imageUrl, summary, url
 
-import csv
+# From Henrique
 import requests
+import csv
 
-response = requests.get("https://api.spaceflightnewsapi.net/v3/articles")
-articles = response.json()
+#pedido ao web service
+response =  requests.get("https://api.spaceflightnewsapi.net/v3/articles")
+#conversao da resposta em json
+artigos = response.json()
 
-print(articles)
+#abertura do ficheiro para escrita
+with open('artigos.csv', 'w',newline = '') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=' ') 
+    #linha de titulo
+    linha = ['ID', 'TITLE', 'IMAGE_URL', 'SUMMARY','URL']
+    #escrita da linha par ao csv
+    spamwriter.writerow(linha)
+    #restantes linhas
+    for f in artigos:
+       # print(f["id"])
+        #print(f["title"])
+        #print(f["imageUrl"])
+        #print(f["summary"])
+        #print(f["url"])
+        linha = [f["id"], f["title"], f["imageUrl"], f["summary"], f["url"]]
+        #escrita da linha para o ficheiro csv 
+        spamwriter.writerow(linha)
+
+
+""" with open('artigos.csv', 'w',newline = '') as csvfile:
+    
+    spamwriter = csv.writer(csvfile, delimiter=' ') 
+
+    for a in artigos:
+
+        line = 
+
+        spamwriter.writerow(fib_line) """
+
+ 
